727c32aea6da1bb480f9198574cbccedb2914f81
NichlasH <nh@jappes.dk>
2016-11-17 11:11:13 +0100

    DB Try

--- www/js/services.js
+++ www/js/services.js
@@ -4,42 +4,42 @@ angular.module('starter.services', [])
     return firebase.auth();
   })
 
-  .factory('Courses', function ($firebaseArray) {
+  .factory('Teachers', function ($firebaseArray) {
     var teacher = $firebaseArray(firebase.database().ref('Teachers'));
     var course = $firebaseArray(firebase.database().ref('Courses'));
     return {
-      add: function (newCourse) {
+      add: function (newTeacher) {
         var updates = {};
         var newId =
-          firebase.database().ref().child('Courses').push().key;
-        updates['/Courses/' + newId] = newCourse;
+          firebase.database().ref().child('Teachers').push().key;
+        updates['/Teachers/' + newId] = newTeacher;
         firebase.database().ref().update(updates);
       },
       all: function () {
         return course;
       },
       remove: function (course) {
-        firebase.database().ref('Courses/' + $id).remove();
+        firebase.database().ref('Teachers/' + $id).remove();
       },
-      get: function (courseId) {
-        for (var i = 0; i < Courses.length; i++) {
-          if (Courses[i].$id === courseId) {
-            return Courses[i];
+      get: function (teacherId) {
+        for (var i = 0; i < teacher.length; i++) {
+          if (teacher[i].$id === teacherId) {
+            return teacher[i];
           }
         }
         return null;
       },
-      addDiary: function (diary) {
+      addDiary: function (teacher) {
         var updates = {};
         var newId =
-          firebase.database().ref().child('diaries').push().ke
+          firebase.database().ref().child('Teachers').push().ke
         y;
-        updates['/diaries/' + newId] = diary;
+        updates['/Teachers/' + newId] = teacher;
         firebase.database().ref().update(updates);
       },
-      getDiaries: function () {
-        diaries.sort(compare);
-        return diaries;
+      getTeachers: function () {
+        teacher.sort(compare);
+        return teacher;
       }
     };
   });
